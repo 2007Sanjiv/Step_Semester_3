cbc4c28c58dd84dfcfa3431ebe644e5d46cebaae
2007Sanjiv <sanjivv2007@gmail.com>
2026-08-29 10:55:09 +0530

    Create p3.java

---
+++ level1/p3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Q3{
+    static void printNumbersUpToN(int n){
+        for(int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        printNumbersUpToN(n);
+    }
+}
