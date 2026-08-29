5d342126ca233812addb343edd1908707e674593
2007Sanjiv <sanjivv2007@gmail.com>
2026-08-29 09:18:19 +0530

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int origNumber = number;
+        int sum = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            sum = sum + digit * digit * digit;
+            number = number / 10;
+        }
+        if (sum == origNumber) {
+            System.out.println("Is the number " + origNumber + " an Armstrong number? true");
+        } else {
+            System.out.println("Is the number " + origNumber + " an Armstrong number? false");
+        }
+    }
+}
